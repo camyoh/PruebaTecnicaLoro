79458b723d4b0e758bb47cbec946a0e00d972d24
Multimedia-7 <multimedia-7@iMac-de-Multimedia-7.local>
2019-10-22 19:05:41 -0500

    First Commit Setup Project

---
+++ PruebaTecnicaLoro/PruebaTecnicaLoro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PruebaTecnicaLoro
+//
+//  Created by Multimedia-7 on 10/22/19.
+//  Copyright © 2019 Carlos Mendieta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
